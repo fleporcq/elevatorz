af6b6cbd257d37eaa9df2e65c2642123abf67454
fleporcq <fleporcq@gmail.com>
2013-10-14 13:04:07 +0200

    reset avec msg

--- src/test/java/elevator/models/ElevatorTest.java
+++ src/test/java/elevator/models/ElevatorTest.java
@@ -8,7 +8,7 @@ public class ElevatorTest {
     @Test
     public void should_be_open(){
         Elevator elevator = Elevator.getInstance();
-        elevator.reset(null);
+        elevator.reset("Elevator should_be_open Test");
         elevator.open();
         Assert.assertTrue(elevator.isOpen());
     }
@@ -16,7 +16,7 @@ public class ElevatorTest {
     @Test
     public void should_be_closed(){
         Elevator elevator = Elevator.getInstance();
-        elevator.reset(null);
+        elevator.reset("Elevator should_be_closed Test");
         elevator.open();
         elevator.close();
         Assert.assertFalse(elevator.isOpen());
@@ -25,7 +25,7 @@ public class ElevatorTest {
     @Test
     public void should_be_clean_after_reset(){
         Elevator elevator = Elevator.getInstance();
-        elevator.reset(null);
+        elevator.reset("Elevator should_be_clean_after_reset Test");
         Assert.assertEquals(elevator.getFloor(), Elevator.LOWER_FLOOR);
         Assert.assertFalse(elevator.isOpen());
         Assert.assertEquals(elevator.getUserCount(), 0);
@@ -38,7 +38,7 @@ public class ElevatorTest {
     @Test
     public void should_throw_already_closed(){
         Elevator elevator = Elevator.getInstance();
-        elevator.reset(null);
+        elevator.reset("Elevator should_throw_already_closed Test");
         try {
             elevator.close();
             Assert.fail();
@@ -50,7 +50,7 @@ public class ElevatorTest {
     @Test
     public void should_throw_already_open(){
         Elevator elevator = Elevator.getInstance();
-        elevator.reset(null);
+        elevator.reset("Elevator should_throw_already_open Test");
         try {
             elevator.open();
             elevator.open();
@@ -63,14 +63,14 @@ public class ElevatorTest {
     @Test
     public void should_be_closed_after_reset(){
         Elevator elevator = Elevator.getInstance();
-        elevator.reset(null);
+        elevator.reset("Elevator should_be_closed_after_reset Test");
         Assert.assertFalse(elevator.isOpen());
     }
 
     @Test
     public void should_throw_already_at_the_lowest_floor(){
         Elevator elevator = Elevator.getInstance();
-        elevator.reset(null);
+        elevator.reset("Elevator should_throw_already_at_the_lowest_floor Test");
         elevator.goTo(0);
         try {
             elevator.moveToDown();
@@ -83,7 +83,7 @@ public class ElevatorTest {
     @Test
     public void should_throw_already_at_the_highest_floor(){
         Elevator elevator = Elevator.getInstance();
-        elevator.reset(null);
+        elevator.reset("Elevator should_throw_already_at_the_highest_floor Test");
         elevator.goTo(5);
         try {
             elevator.moveToUp();
@@ -96,7 +96,7 @@ public class ElevatorTest {
     @Test
     public void should_throw_is_going_outside(){
         Elevator elevator = Elevator.getInstance();
-        elevator.reset(null);
+        elevator.reset("Elevator should_throw_is_going_outside Test");
         try {
             elevator.goTo(-1);
             Assert.fail();
